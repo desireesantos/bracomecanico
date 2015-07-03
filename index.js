efa19e12ef2dffa71b0fa0d91e1e0ba0b2fd4720
Desiree Santos <desiree.santos.001@acad.pucrs.com>
2015-07-03 20:20:48 -0300

    Adding jonny five

---
+++ index.js
@@ -0,0 +1,24 @@
+var narf = require('narf');
+var five = new require('johnny-five');
+var board = new require('johnny-five').Board();
+console.log("Starting ..");
+board.on('ready', function() {
+  var bottonServo = new five.Servo(5);
+  var APIFunctions = {
+    POST : {
+      mecanicArm : function (data, callback ){
+        console.log(data.url.value);
+      }
+    }};
+
+  var hs = new narf.HttpServer().start( 8080 );
+  hs.on( 'port', function( port ){
+    console.log('Start server on port', port);
+    hs.addAPI( { functions : APIFunctions } );
+  } );
+});
+
+narf.pageServer({ 
+  port: 8000,
+  path: __dirname + '/client/'
+});
